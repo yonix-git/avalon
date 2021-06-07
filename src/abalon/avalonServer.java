10ac814aa3067914950a09a3f5901604a636952b
yoni d. <yoni2435garbage@gmail.com>
2021-06-07 23:34:51 +0300

    initial commit

--- src/abalon/avalonServer.java
+++ src/abalon/avalonServer.java
@@ -1,6 +1,5 @@
 package abalon;
 
-
 import myClasses.board;
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -10,32 +9,31 @@ import java.net.Socket;
 this class has been created to make a serverSocket,
 it's accept to two clients and open thread for tham
  */
-
 /**
  *
  * @author user
  */
 public class avalonServer {
-    
-        private board B = new board();
-        
+
+    private board B = new board();
+
     public void server() throws IOException {
-        
+
         ServerSocket myServer = null;
-        
+
         try {
             myServer = new ServerSocket(5845);
         } catch (Exception e) {
             System.out.println("go fuck yourself");
         }
-        
+
         Socket mySocket1, mySocket2;
         while (true) {
             mySocket1 = myServer.accept();
             System.out.println("n1 is ready");
             mySocket2 = myServer.accept();
             System.out.println("n2 is ready");
-
+            
             thread T = new thread(mySocket1, mySocket2);
         }
     }
